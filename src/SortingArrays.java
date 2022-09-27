dd537a9974ff006a698d2f0e079dacf91fbecb98
KevimLSDS <kevim.lhouis@outlook.com>
2022-09-27 19:24:30 -0300

    First challenge of sorting algos

---
+++ src/SortingArrays.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class SortingArrays {
+    public static void main(String[] args) {
+
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        Arrays.sort(intArray);
+        Arrays.parallelSort(intArray);
+
+        for (int number : intArray) {
+            System.out.println(number);
+        }
+    }
+}
---
+++ src/sorting/challenges/FirstChallenge.java
@@ -0,0 +1,51 @@
+package sorting.challenges;
+
+public class FirstChallenge {
+
+    // Do a merge sort in descending order
+
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        mergeSort(intArray, 0, intArray.length);
+
+        for (int number : intArray) {
+            System.out.println(number);
+        }
+
+    }
+
+    public static void mergeSort(int[] input, int startIndex, int endIndex) {
+        if (endIndex - startIndex < 2) {
+            return;
+        }
+
+        int midIndex = (startIndex + endIndex) / 2;
+
+        mergeSort(input, startIndex, midIndex);
+        mergeSort(input, midIndex, endIndex);
+        merge(input, startIndex, midIndex, endIndex);
+
+
+    }
+
+    private static void merge(int[] input, int startIndex, int midIndex, int endIndex) {
+        if (input[midIndex - 1] >= input[midIndex]) {
+            return;
+        }
+
+        int i = startIndex;
+        int j = midIndex;
+        int tempIndex = 0;
+
+        int[] temp = new int[endIndex - startIndex];
+
+        while (i < midIndex && j < endIndex) {
+            temp[tempIndex++] = input[i] >= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, startIndex + tempIndex, midIndex - i);
+        System.arraycopy(temp, 0, input, startIndex, tempIndex);
+
+    }
+}
